7439e85afef21d832a99c9913f94428374b58bb7
WorkerHarder171 <dadybima171@gmail.com>
2024-07-16 12:41:09 +0700

    update all 16/07/2024

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from "vite";
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  		define: {
+			"process.env": env,
+		},
+  plugins: [react(), compression()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -14,5 +19,12 @@ export default defineConfig({
     rollupOptions: {
       external: ['react-redux','@reduxjs/toolkit','react-icons/bs']
     }
-  }
+  },server: {
+			watch: {
+				usePolling: true,
+			},
+			host: true,
+			strictPort: false,
+			port: env.PORT || 5173,
+		},
 });
