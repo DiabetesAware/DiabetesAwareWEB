d82fc74c9368ce6ccf5e9096b3d7af7c853bc9cb
WorkerHarder171 <dadybima171@gmail.com>
2024-07-16 12:37:24 +0700

    update all 16/07/2024

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react-redux','@reduxjs/toolkit']
+      external: ['react-redux','@reduxjs/toolkit','react-icons/bs']
     }
   }
 });
